8f88f3a3febfbbc98fe4b6f8af34ba83c0014488
YONATANE MEKETE <yonatanemekete33@gmail.com>
2024-07-10 22:58:26 -0700

    setup done

---
+++ src/assets/Icons/Icons.ts
@@ -0,0 +1,43 @@
+import react from './react-color.svg';
+import typescript from './typescript-color.svg';
+import javascript from './javascript-color.svg';
+import next from './nextdotjs-color.svg';
+import git from './git-color.svg';
+import github from './github-color.svg';
+import linkidin from './linkedin-color.svg';
+import nextui from './nextui-color.svg';
+import npm from './npm-color.svg';
+import reacthookform from './reacthookform-color.svg';
+import reactquery from './reactquery-color.svg';
+import tailwind from './tailwind-color.svg';
+import reactrouter from './reactrouter-color.svg';
+import shadcn from './shadcnui-color.svg';
+import telegram from './telegram-color.svg';
+import vercel from './vercel-color.svg';
+import vite from './vite-color.svg';
+import vitest from './vitest-color.svg';
+import x from './x-color.svg';
+import gsap from './greensock-color.svg';
+
+export {
+  react,
+  typescript,
+  javascript,
+  next,
+  git,
+  github,
+  linkidin,
+  nextui,
+  npm,
+  reacthookform,
+  reactquery,
+  tailwind,
+  reactrouter,
+  shadcn,
+  telegram,
+  vercel,
+  vite,
+  vitest,
+  x,
+  gsap,
+};
