f0d8030fc6eeac0bdf657ad08e28432abf34d544
YONATANE MEKETE <yonatanemekete33@gmail.com>
2024-07-13 19:13:38 -0700

    Footer

---
+++ src/Components/Footer.tsx
@@ -0,0 +1,50 @@
+import { github, linkidin, telegram, x } from '@/assets/Icons/Icons';
+
+const Footer = () => {
+  return (
+    <div className="mt-36 h-[50vh] w-full max-w-[1300px] mx-auto">
+      <div className=" text-text text-5xl font-header font-extrabold text-center mb-6">
+        YONATANE <span className="text-accent">MEKETE</span>
+      </div>
+      <div className="border-b border-textgrey w-[70vw] max-w-[1100px] mx-auto mb-2"></div>
+
+      <div className="flex items-center justify-between w-[70vw] max-w-[1100px] mx-auto">
+        <p className="text-textgrey text-base font-body font-semibold">
+          Copyright © 2022. All right reserved
+        </p>
+        <div className="flex items-center gap-x-4">
+          <div className="size-[2rem] p-2 bg-bg rounded-full shadow-sm shadow-accent cursor-pointer">
+            <img
+              src={github}
+              alt="social"
+              className="size-full object-cover object-center"
+            />
+          </div>
+          <div className="size-[2rem] p-2 bg-bg rounded-full shadow-sm shadow-accent cursor-pointer">
+            <img
+              src={x}
+              alt="social"
+              className="size-full object-cover object-center"
+            />
+          </div>
+          <div className="size-[2rem] p-2 bg-bg rounded-full shadow-sm shadow-accent cursor-pointer">
+            <img
+              src={linkidin}
+              alt="social"
+              className="size-full object-cover object-center rounded-full"
+            />
+          </div>
+          <div className="size-[2rem] p-2 bg-bg rounded-full shadow-sm shadow-text cursor-pointer">
+            <img
+              src={telegram}
+              alt="social"
+              className="size-full object-cover object-center"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -2,14 +2,16 @@ import AboutMe from '@/Components/AboutMe';
 import Hero from '../Components/Hero';
 import Navigation from '../Components/Navigation';
 import Projects from '@/Components/Projects';
+import Footer from '@/Components/Footer';
 
 const Home = () => {
   return (
-    <div className="bg-bg pb-[100vh]">
+    <div className="bg-bg">
       <Navigation />
       <Hero />
       <AboutMe />
       <Projects />
+      <Footer />
     </div>
   );
 };
